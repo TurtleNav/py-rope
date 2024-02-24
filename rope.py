cb98250dc9b7520e7930f3afb68e81f85eda21d1
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-23 19:01:18 -0500

    Added some property getters/setters

--- rope.py
+++ rope.py
@@ -1,6 +1,32 @@
 
+class _Rope
 
 class Rope:
+
+    def __init__(self, value, weight, length):
+        self._value = value
+        self._weight = weight
+        self._length = length
+
+        self._left = None
+        self._right = None
+
+    @property
+    def is_leaf(self):
+        return self._left is None
+    
+    @property
+    def length(self):
+        return self._length or 0
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, v):
+        self._value = v
+
     @classmethod
     def concat(self, other):
         return NotImplemented
