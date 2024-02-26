ca232b45d58f0b3b72997cce2dc929504e593c5f
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-25 23:00:47 -0500

    workin on going through the string methods that need to be implemented for rope. Aside, refactoring will be necessary

--- rope.py
+++ rope.py
@@ -4,17 +4,21 @@ from reprlib import recursive_repr
 
 class Rope:
 
-    def __init__(self, data, left=None, right=None):
-        self._data = data
+    def __init__(self, data=None, left=None, right=None):
         self._weight = 0
+        self.data = data
+        self.left = left
+        self.right = right
+    
+    @property
+    def data(self):
+        return self._data
+    
+    @data.setter
+    def data(self, data):
+        self._data = data
         if data is not None:
             self._weight += len(data)
-        if left is not None:
-            self._weight += len(left)
-        if right is not None:
-            self._weight += len(right)
-        self._left = left
-        self._right = right
 
     @property
     def is_leaf(self):
@@ -27,13 +31,25 @@ class Rope:
     @property
     def left(self):
         return self._left
+    
+    @left.setter
+    def left(self, left):
+        self._left = left
+        if left is not None:
+            self._weight += len(left)
 
     @property
     def right(self):
         return self._right
 
+    @right.setter
+    def right(self, right):
+        self._right = right
+        if right is not None:
+            self._weight += len(right)
+
     def __len__(self):
-        return len(self._data)
+        return self.weight
 
     def concat(self, other):
         if self._data is None:
@@ -45,8 +61,44 @@ class Rope:
     def delete(self, start, length):
        return 
 
+    """
+    Splitting the rope requires consideration of special cases and recursion
+    """
     def split(self, index):
-        return NotImplemented
+
+        if index == 0:
+            return None, self
+        
+        if index == self.weight:
+            return self, None
+
+        if index == self.left.weight:
+            return self.left, self.right
+
+        if index < self.left.weight:
+
+
+
+            re_left = Rope(self.left.data[:index])
+
+
+
+
+            if self.left.isleaf:
+                re_left = Rope(self.left._data[:index])
+                re_right = self.right.concat()
+            new_left = Rope(self._data[:index])
+            return Rope(self._data[:index]), self.concat(self.right)
+
+        if index - 1 == self.weight:
+            # If the requested split index is at the final index of the rope,
+            # we either return the root rope / left and right
+            if self.is_leaf:
+                return self, self.right
+            
+            return self.left, self.right
+        
+        rope = Rope()
 
     def insert(self, index, string):
         return NotImplemented
@@ -63,6 +115,7 @@ class Rope:
 
         if index > self.left.weight:
             return self.right.index(self.weight - index)
+
         return self.left.index(index)
 
     @recursive_repr()
@@ -73,3 +126,54 @@ class Rope:
         if self._right is not None:
             it.append(str(self._right))
         return "".join(it)
+    
+    def __iter__(self):
+        yield self
+
+        if not self.is_leaf:
+            yield from self.left
+            yield from self.right
+    
+    def _copy(self, cb):
+        node = Rope()
+        if self.is_leaf:
+            node.data = cb(self.data)
+        else:
+            node.left = cb(self.left)
+            node.right = cb(self.right)
+
+    def capitalize(self):
+        node = Rope()
+        if self.is_leaf:
+            node.data = self.data.capitalize()
+        else:
+            node.left = self.left.capitalize()
+            node.right = self.right.capitalize()
+        return node
+    
+    def casefold(self):
+        node = Rope()
+        if self.is_leaf:
+            node.data = self.data.casefold()
+        else:
+            node.left = self.left.casefold()
+            node.right = self.right.casefold()
+        return node
+    
+    def center(self, width, fillchar=" "):
+        if width <= self.weight:
+            return self  # TODO: return copy??
+        
+        new_root = Rope()
+
+        new_root.left = Rope(
+            left=Rope(fillchar * ((width - self.weight + 1) // 2)),
+            right=self.left
+            )
+        
+        new_root.right = Rope(
+            left=self.right,
+            right=Rope(fillchar * ((width - self.weight) // 2))
+        )
+        
+        return new_root
